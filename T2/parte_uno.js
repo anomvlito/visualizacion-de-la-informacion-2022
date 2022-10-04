80bf17717c3d8d38d5e78dc89deb8632a6f80d98
anomvlito <84647703+anomvlito@users.noreply.github.com>
2022-10-04 11:17:17 -0300

    esta bbola

--- T2/parte_uno.js
+++ T2/parte_uno.js
@@ -1,34 +1,122 @@
-// Creamos un SVG en body.
-const svg = d3.select("body").append("svg");
+function createVis1(array, rute) {
 
-// Creamos una función que se encarga de actualizar el SVG según los datos que llegan.
-function joinDeDatos1(datos) {
-  // Definimos el ancho y largo del SVG.
-  svg.attr("width", 50 + datos.length * 100).attr("height", 500);
+  const SVG = d3.select(rute)
+    .append("svg")
+    .attr("width", 1000)
+    .attr("height", 400);
 
-  // Vinculamos los datos con cada elemento rect con el comando join.
-  const enter_and_update = svg.append("g")
-    .selectAll("g")
-    .data(datos)
-    .join((enter) => enter{
-        let g = enter
+  const domain = [...Array(7).keys()]; // Creamos una lista de 0 al 9
 
-        g.append('g')
+    // Definimos escala de bandas para poner cada dato.
+  const escalaX = d3
+      .scaleBand()
+      .domain(domain)
+      .rangeRound([0, 1000])
+      .padding(1); // agregar sepación entre el final y el inicio de una banda.
 
-        g.append("rect")
-        .attr("width", (d) => (d.Artwork/1000))
-        .attr("fill", "orange")
-        .attr("height", (d) => (d.Artwork/100))
-        .attr("x", (_, i) => 50 + i * 80)
-        },
-        (update) => update,
-        (exit) => exit
-        .transition()
-        .duration(1000)
-        .attr("height", 0)
-        .remove() 
-    )
-}
+
+  const escalaPorcentaje = d3
+      .scaleLinear()
+      .domain([0,100])
+      .rangeRound([0, 30])
+      // .padding(1); // agregar sepación entre el final y el inicio de una banda.
+
+  const escalaY = d3
+      .scaleLog()
+      .domain([1, d3.max(array, d => d.Artwork)])
+      .range([0, 100])
+
+
+  const grupos = SVG
+      .selectAll("g")
+      .data(array, d => d.Artist)
+      .join(enter => {
+          // Creamos un grupo "g"
+          const grupos = enter.append("g")
+          ;
+
+          // Para cada grupo, le agregamos un círculo
+
+
+          // Para cada grupo, le agregamos un cuadrado
+          const sizeRect = 40
+          grupos.append("rect")
+          .attr('width', d => escalaY(d.Artwork))
+          .attr('height', d => escalaY(d.Artwork))
+          .attr('x', 0)
+          .attr('y', 0)
+          .attr('fill', "white")
+          .attr('stroke', "green")
+          .attr('stroke-width', d => d.Artist/ 200);
+
+          // grupos.append("circle")
+          //     .attr('r', d => d.Female)
+          //     .attr('cx', d => escalaY(d.Artwork)/2)
+          //     .attr('cy', d => escalaY(d.Artwork)/2)
+          //     .attr('fill', "blue")
+          // // Para cada grupo, le agregamos una barrita
+
+          const barra = grupos.append("g");
+
+          barra.append("rect")
+              .attr('class', 'barra')
+              .attr('width', 15)
+              .attr('height', d => escalaPorcentaje(d.Male))
+              .attr('x', d => escalaY(d.Artwork)/2 - 10)
+              .attr('y', d =>escalaY(d.Artwork)/2 -10)
+              .attr('fill', "orange")
+          barra.append("rect")
+              .attr('class', 'barra')
+              .attr('width', 15)
+              .attr('height', d => escalaPorcentaje(d.Female))
+              .attr('x', d => escalaY(d.Artwork)/2 - 10)
+              .attr('y', d =>escalaY(d.Artwork)/2 -10 - escalaPorcentaje(d.Female))
+              .attr('fill', "green")
+
+          // // Para cada grupo, le agregamos un texto
+          const text2 = grupos
+              .append("text")
+              .attr("x", 10)
+              .attr("y", 40)
+              .style("font-size", "10px")
+              .text("")
+
+          grupos.append("text")
+              .attr('x', 25)
+              .attr('y',-20)
+              .style("dominant-baseline", "middle")
+              .style("text-anchor", "middle")
+              .style("font-size", "10px")
+              .text(d => d.Category)
+
+          // retornamos nuestros grupo ques aprovechamos de aplicar una traslación a
+          // cada uno en el eje X.
+
+          grupos.on('mouseover', (d) => {
+            const porcentajes = `${d.Male}`
+            d3.select("g").append("text")
+            .attr('x', -25)
+            .attr('y', 100)
+            .attr("class", "mylabel")
+            .style("font-size", "10px")
+            .text(porcentajes)
+            console.log("Your mouse went over")
+            console.log(this.parentNode)
+              // .text2.text(`Posición en el SVG ${d.Male}-${d => d.Female}`)
+                
+                // .attr('stroke-width',3)
+            })
+          grupos.on('mouseout',(d) => {
+              d3.selectAll(".mylabel").remove()
+              console.log("Your mouse salio")
+                // .text2.text("")
+            })
+
+          return grupos.attr("transform", (_, i) => `translate(${escalaX(i)}, 40)`)
+      })
+
+
+  }
 
 
 const parseo1 = (d) => (
@@ -38,8 +126,8 @@ const parseo1 = (d) => (
     Category: d["Category"],
     Artist: parseInt(d.Artist),
     Artwork: parseInt(d.Artwork),
-    Male: parseInt(d.Male),
-    Female: parseInt(d.Female),
+    Male: parseInt((+d.Male/(+d.Male + +d.Female)) * 100),
+    Female: Math.abs( 100 - parseInt((+d.Male/(+d.Male + +d.Female)) * 100)),
   });
 
 const parseo2 = (d) => (
@@ -47,27 +135,25 @@ const parseo2 = (d) => (
     Artist: d.Artist,
     Nacionality: d.Nacionality,
     Gender: d.Gender,
-    BirthYear: parseInt(d["BirthYear"]),   
+    BirthYear: parseInt(d["BirthYear"]),
     DeathYear: parseInt(d.DeathYear),
     TotalArtwork: parseInt(d.TotalArtwork),
     Categories: JSON.parse((d["Categories"].replace(/^"|"$/g, ''))),
   });
 
 function runCode() {
-    
-    // const BASE_URL = "https://gist.githubusercontent.com/Hernan4444/";
-    // const URL = BASE_URL + "16a8735acdb18fabb685810fc4619c73/raw/d16677e2603373c8479c6535df813a731025fd4a/ArtistProcessed.csv"
-    
+
     const URL1= "https://gist.githubusercontent.com/Hernan4444/16a8735acdb18fabb685810fc4619c73/raw/d16677e2603373c8479c6535df813a731025fd4a/CategoryProcessed.csv"
     const URL2 = "https://raw.githubusercontent.com/jeschuwirth/base-de-datos-infovis/4b3a8728d1d768ef69bf4535cb516d122040fa1f/T2/ArtistProcessed.csv"
-    
+
     d3.csv(URL1, parseo1)
       .then((datos) => {
         console.log(datos);
-        joinDeDatos1(datos);
+        createVis1(datos, "#vis-1")
+        // joinDeDatos1(datos);
       })
       .catch((error) => console.log(error));
-    
+
     d3.csv(URL2, parseo2)
       .then((datos) => {
         console.log(datos);
